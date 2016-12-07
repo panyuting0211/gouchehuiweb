e50ee3d1ac7def46687acec5f3d93d10b52b88e5
panyuting <2224679347@qq.com>
2016-12-07 09:24:34 +0800

    all thinkphp

---
+++ ThinkPHP/Library/Think/Pay/Driver/Weixin.class.php
@@ -0,0 +1,255 @@
+<?php
+namespace Think\Pay\Driver;
+use Think\Pay\Driver\Qrcode;
+
+class Weixin extends \Think\Pay\Pay {
+    protected $gateway = 'https://api.mch.weixin.qq.com/pay/unifiedorder';
+    protected $verify_url = 'http://notify.alipay.com/trade/notify_query.do';
+    protected $configs = array(
+        'AppID'=>'wxe8a079d4b6a764cc',
+        'AppSecret'=>'92b113bc6cc0ad1233dae1a064fd6811',
+
+    );
+
+    public function check() {
+
+        if (!$this->configs['AppID'] || !$this->configs['AppSecret']) {
+            E("微信设置有误！");
+        }
+        return true;
+    }
+
+    public function buildRequestForm(\Think\Pay\PayVo $vo) {
+
+        $param = array(
+            'appid'=>'wxe8a079d4b6a764cc',
+            'mch_id'=>'1269870801' ,
+            'device_info'=>'WEB',
+            'nonce_str'=> rand(),
+            'body' =>$vo['title'],
+            'out_trade_no' => $vo['out_trade_no'],
+            'total_fee' => $vo['money']*100,
+            'spbill_create_ip'=>$vo['pay_ip'],
+            'notify_url' => 'http://test3.gouchehui.com:8082/index.php/Pay/wxnotify',
+            'trade_type'=>'NATIVE',
+        );
+        $param['sign']= $this->MakeSign($param);
+        $xml=$this->ToXml($param);
+        $response = self::postXmlCurl($xml, $this->gateway, false,30);
+        $array=$this->FromXml($response);
+        return $array;
+    }
+
+    /**
+     * 生成签名
+     * @return 签名，本函数不覆盖sign成员变量，如要设置签名需要调用SetSign方法赋值
+     */
+    public function MakeSign($arr)
+    {
+        //签名步骤一：按字典序排序参数
+        ksort($arr);
+        $string = $this->ToUrlParams($arr);
+        //签名步骤二：在string后加入KEY
+        $string = $string . "&key=4ddb4176fbbc2adbd56aa31965adf6cf";
+        //签名步骤三：MD5加密
+        $string = md5($string);
+        //签名步骤四：所有字符转为大写
+        $result = strtoupper($string);
+        return $result;
+    }
+
+    /**
+     * 格式化参数格式化成url参数
+     */
+    public function ToUrlParams($arr)
+    {
+        $buff = "";
+        foreach ($arr as $k => $v)
+        {
+            if($k != "sign" && $v != "" && !is_array($v)){
+                $buff .= $k . "=" . $v . "&";
+            }
+        }
+        $buff = trim($buff, "&");
+        return $buff;
+    }
+
+    /**
+     * 输出xml字符
+     * @throws WxPayException
+     **/
+    public function ToXml($arr)
+    {
+        if(!is_array($arr) || count($arr) <= 0)
+        {
+            E("数组数据异常！");
+        }
+        $xml = "<xml>";
+        foreach ($arr as $key=>$val)
+        {
+            if (is_numeric($val)){
+                $xml.="<".$key.">".$val."</".$key.">";
+            }else{
+                $xml.="<".$key."><![CDATA[".$val."]]></".$key.">";
+            }
+        }
+        $xml.="</xml>";
+        return $xml;
+    }
+
+    /**
+     * 将xml转为array
+     * @param string $xml
+     * @return bool|\mix|mixed|string
+     */
+    public function FromXml($xml)
+    {
+        if(!$xml){
+            E("xml数据异常！");
+        }
+        //将XML转为array
+        //禁止引用外部xml实体
+        libxml_disable_entity_loader(true);
+       $arr = json_decode(json_encode(simplexml_load_string($xml, 'SimpleXMLElement', LIBXML_NOCDATA)), true);
+        return $arr;
+    }
+
+    /**
+     * 以post方式提交xml到对应的接口url
+     * @param string $xml 需要post的xml数据
+     * @param string $url url
+     * @param bool $useCert 是否需要证书，默认不需要
+     * @param int $second url执行超时时间，默认30s
+     * @return mixed
+     * @throws WxPayException
+     */
+    private static function postXmlCurl($xml, $url, $useCert = false, $second = 30)
+    {
+        $ch = curl_init();
+        //设置超时
+        curl_setopt($ch, CURLOPT_TIMEOUT, $second);
+
+        //如果有配置代理这里就设置代理
+        if(C('CURL_PROXY_HOST') != "0.0.0.0"
+            && C('CURL_PROXY_PORT') != 0){
+            curl_setopt($ch,CURLOPT_PROXY, C('CURL_PROXY_HOST'));
+            curl_setopt($ch,CURLOPT_PROXYPORT, C('CURL_PROXY_PORT'));
+        }
+        curl_setopt($ch,CURLOPT_URL, $url);
+        curl_setopt($ch,CURLOPT_SSL_VERIFYPEER,FALSE);
+        curl_setopt($ch,CURLOPT_SSL_VERIFYHOST,FALSE);//严格校验
+        //设置header
+        curl_setopt($ch, CURLOPT_HEADER, FALSE);
+        //要求结果为字符串且输出到屏幕上
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+
+        if($useCert == true){
+            //设置证书
+            //使用证书：cert 与 key 分别属于两个.pem文件
+            curl_setopt($ch,CURLOPT_SSLCERTTYPE,'PEM');
+            curl_setopt($ch,CURLOPT_SSLCERT, C('SSLCERT_PATH'));
+            curl_setopt($ch,CURLOPT_SSLKEYTYPE,'PEM');
+            curl_setopt($ch,CURLOPT_SSLKEY, C('SSLKEY_PATH'));
+        }
+        //post提交方式
+        curl_setopt($ch, CURLOPT_POST, TRUE);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $xml);
+        //运行curl
+        $data = curl_exec($ch);
+        //返回结果
+        if($data){
+            curl_close($ch);
+            return $data;
+        } else {
+            $error = curl_errno($ch);
+            curl_close($ch);
+            E("curl出错，错误码:$error");
+        }
+    }
+
+/********************************************************下面方法暂时无用**************************************************************************/
+
+    /**
+     * 获取返回时的签名验证结果
+     * @param $sign 返回的签名结果
+     * @return 签名验证结果
+     */
+    protected function getSignVeryfy($param, $sign) {
+        //除去待签名参数数组中的空值和签名参数
+        $param_filter = array();
+        while (list ($key, $val) = each($param)) {
+            if ($key == "sign" || $key == "sign_type" || $val == "") {
+                continue;
+            } else {
+                $param_filter[$key] = $param[$key];
+            }
+        }
+
+        ksort($param_filter);
+        reset($param_filter);
+
+        //把数组所有元素，按照“参数=参数值”的模式用“&”字符拼接成字符串
+        $prestr = "";
+        while (list ($key, $val) = each($param_filter)) {
+            $prestr.=$key . "=" . $val . "&";
+        }
+        //去掉最后一个&字符
+        $prestr = substr($prestr, 0, -1);
+
+        $prestr = $prestr . $this->configs['key'];
+        $mysgin = md5($prestr);
+
+        if ($mysgin == $sign) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * 针对notify_url验证消息是否是支付宝发出的合法消息
+     * @return 验证结果
+     */
+    public function verifyNotify($notify) {
+        //生成签名结果
+        $isSign = $this->getSignVeryfy($notify, $notify["sign"]);
+        //获取支付宝远程服务器ATN结果（验证是否是支付宝发来的消息）
+        $responseTxt = 'true';
+        if (!empty($notify["notify_id"])) {
+            $responseTxt = $this->getResponse($notify["notify_id"]);
+        }
+        if (preg_match("/true$/i", $responseTxt) && $isSign) {
+            $this->setInfo($notify);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    protected function setInfo($notify) {
+        $info = array();
+        //支付状态
+        $info['status'] = ($notify['trade_status'] == 'TRADE_FINISHED' || $notify['trade_status'] == 'TRADE_SUCCESS') ? true : false;
+        $info['money'] = $notify['total_fee'];
+        $info['out_trade_no'] = $notify['out_trade_no'];
+        $this->info = $info;
+    }
+
+    /**
+     * 获取远程服务器ATN结果,验证返回URL
+     * @param $notify_id 通知校验ID
+     * @return 服务器ATN结果
+     * 验证结果集：
+     * invalid命令参数不对 出现这个错误，请检测返回处理中partner和key是否为空
+     * true 返回正确信息
+     * false 请检查防火墙或者是服务器阻止端口问题以及验证时间是否超过一分钟
+     */
+    protected function getResponse($notify_id) {
+        $partner = $this->configs['partner'];
+        $veryfy_url = $this->verify_url . "?partner=" . $partner . "&notify_id=" . $notify_id;
+        $responseTxt = $this->fsockOpen($veryfy_url);
+        print_r($responseTxt);exit;
+        return $responseTxt;
+    }
+
+}
